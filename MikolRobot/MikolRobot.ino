502d9f2e4525d0709eb4d7c6934b0a53c2f12a37
Mikol Aspinwall <maaspinwall@goshen.edu>
2017-11-20 10:40:11 -0500

    rewrote everything

--- MikolRobot/MikolRobot.ino
+++ MikolRobot/MikolRobot.ino
@@ -27,25 +27,7 @@ int range;          // distance (in cm) to object sensed by rangefinder
 int angle = 90;     // angle of servo that aims rangefinder (0-180) 90 is straight ahead
 
 // ----------------------------------------------------------------------------------
-void setup() {
-  // Here is where you put lines that execute just once.  Like setting input and output.
-  m.begin();  // this starts up the menu
-  m.addItem("1 Wheel Speeds", &setWheelSpeed);    //  Here is where you put the menu items.
-  m.addItem("2 Move Robot  ", &moveRobot);       //  For each menu item, you give the text of the menu label
-  m.addItem("3 Range Finder", &testRanger);     //  and then the name of the function to be called when you select it
-  m.addItem("4 Move Servo  ", &testServo);     
-  m.addItem("5 Adjust someN", &setValue);  
-  m.addItem("6 Follower    ", &Follower);   
-  m.addItem("7 Sweep       ", &Sweep);
-  m.addItem("8 Chicken     ", &Chicken);
 
-
-  m.lcd.setBacklight(0x1);     // Turns on the backlight.  Will be done by default in next version of physMenu
-  pinMode(3, OUTPUT);  // Rangefinder trigger
-  pinMode(4, INPUT);   // Rangefinder response
-  AFMS.begin();  // Initialize the motor shield
-  rangerServo.attach(2);  // Associates rangerServo with pin 2
-}
 
 // -----------------------------------------------------------------------------------
 void loop() {
@@ -82,9 +64,11 @@ void setWheelSpeed() {
   }
 }  
 // Follower
+/*
 void Follower() {
   m.lcd.clear();
   int prev;
+  int idealdir;
   leftWheel->setSpeed(100);
   rightWheel->setSpeed(100);
   while(1) {
@@ -95,8 +79,23 @@ void Follower() {
       rightWheel->run(BACKWARD);
     }
     else if (distance > 55) {
-      leftWheel->run(FORWARD);
-      rightWheel->run(FORWARD);
+      idealdir = Sweep();
+      if (idealdir == 0) {
+        while(Sweep() != 2) {
+        // go right
+          leftWheel->run(FORWARD);
+          rightWheel->run(BACKWARD);
+        }
+      } else if (idealdir == 1) {
+        while(Sweep() != 2) {
+          // go left
+          leftWheel->run(BACKWARD);
+          rightWheel->run(FORWARD);
+        }
+      } else if (idealdir == 2) {
+        leftWheel->run(FORWARD);
+        rightWheel->run(FORWARD);
+      }
     }
     else if (distance >= 45 && distance <= 55) {
       leftWheel->run(RELEASE);
@@ -110,8 +109,172 @@ void Follower() {
 
   }
 }
+char* names[] = {"Alice", "Bob", "Charlie", "Delta", "Epsilon"};
+
+struct ValueKeeper {
+  int array[3];
+};
+
+struct PhysicalObject {
+  String name;
+  int tracking;
+  // 0 for lost, 1 for tracking
+  int ant;
+  
+  // short for anticipation which is a long word
+  // 0 for left, 1 for none, 2 for right, for consistency. probably won't implement this anytime soon
+};
+
+void Follower() {
+  int turnTime = 400;
+  struct PhysicalObject obj;
+  m.lcd.clear();
+  // first time "setup" is different, hence why this first is out of the loop
+  int probabledirection = Decider(Looker());
+  obj.ant = probabledirection;
+  obj.tracking = 1;
+  obj.name = names[random(5)];
+  m.lcd.print("Tracking ");
+  m.lcd.print(obj.name);
+  leftWheel->run(FORWARD);
+  rightWheel->run(FORWARD);
+  if (probabledirection == 0) {
+    leftWheel->run(BACKWARD);
+    rightWheel->run(FORWARD);
+    delay(turnTime);
+    leftWheel->run(RELEASE);
+    rightWheel->run(RELEASE);
+  } else if (probabledirection == 1) {
+    leftWheel->run(FORWARD);
+    rightWheel->run(FORWARD);
+  } if (probabledirection == 2) {
+    leftWheel->run(FORWARD);
+    rightWheel->run(BACKWARD);
+    delay(turnTime);
+    leftWheel->run(FORWARD);
+    rightWheel->run(FORWARD);
+  }  
+  while(1) {
+    int probabledirection = Decider(Looker());
+    if (probabledirection == 0) {
+      leftWheel->run(BACKWARD);
+      rightWheel->run(FORWARD);
+      delay(turnTime);
+      leftWheel->run(RELEASE);
+      rightWheel->run(RELEASE);
+    } else if (probabledirection == 1) {
+      leftWheel->run(FORWARD);
+      rightWheel->run(FORWARD);
+    } if (probabledirection == 2) {
+      leftWheel->run(FORWARD);
+      rightWheel->run(BACKWARD);
+      delay(turnTime);
+      leftWheel->run(RELEASE);
+      rightWheel->run(RELEASE);
+    }
+    leftWheel->run(FORWARD);
+    rightWheel->run(FORWARD);
+  }
+}
+
+struct ValueKeeper Looker() {
+  ValueKeeper values;
+  rangerServo.write(115);
+  delay(500);
+  values.array[0] = getRange();
+  rangerServo.write(90);
+  delay(500);
+  values.array[1] = getRange();
+  rangerServo.write(65);
+  delay(500);
+  values.array[2] = getRange();
+  rangerServo.write(90);
+  delay(300);
+  return values;
+}
+
+int Decider(struct ValueKeeper values) {
+  int directions[3]={values.array[0], values.array[1], values.array[2]};
+  int biggest;
+  biggest = 0;
+  int probableTarget;
+  for (int i = 0; i < 3; i++) {
+    if (directions[i] > 70) {
+      directions[i] = -1;
+    }
+    if (directions[i] < 70 && directions[i] > 15) {
+      probableTarget = i;
+    }
+  }
+  return probableTarget;
+  /*for (int i = 0; i < 3; i++) {
+    if (values.array[i] > values.array[biggest]) {
+      biggest = i;
+    }
+    if (values.array[i] > 45 && values.array[i] < 55) {
+      probableTarget = i; 
+    }
+  }
+  if (values.array[0] == values.array[1] && values.array[1] == values.array[2]) {
+    return 1;
+    // if all the readings are the same (lost), go forward?
+  }
+  if (probableTarget == biggest) {
+    return probableTarget;
+  } else {
+    if (biggest > probableTarget) {
+      return biggest;
+    }
+  }
+  */
 
 
+void Follower() {
+  leftWheel->setSpeed(100);
+  rightWheel->setSpeed(100);
+  int values[3]; 
+  m.lcd.clear();
+  while(1) {
+    rangerServo.write(115);
+    delay(150);
+    values[0] = getRange();
+    rangerServo.write(90);
+    delay(150);
+    values[1] = getRange();
+    rangerServo.write(65);
+    delay(150);
+    values[2] = getRange();
+    m.lcd.clear();
+    m.lcd.print(values[0]);
+    m.lcd.print("  ");
+    m.lcd.print(values[1]);
+    m.lcd.print("  ");
+    m.lcd.print(values[2]);
+    if (values[0] > (values[1] - 20) && values[0] < (values[1] + 20) && values[0] > (values[2]-20) && values[0] < (values[2]+20) && values[1] > 15) {
+      leftWheel->run(FORWARD);
+      rightWheel->run(FORWARD);
+      // this is the nothing case, no imminent objects
+    } else if (values[1] < 15) {
+      leftWheel->run(BACKWARD);
+      rightWheel->run(BACKWARD);
+      // this is the something is right in front of you case
+    } else if (values[0] < values[1] && values[0] < values[2] && values[0] < 70) {
+      // whatever's on the left is the closest
+      leftWheel->run(BACKWARD);
+      rightWheel->run(FORWARD);
+      delay(250);
+      leftWheel->run(FORWARD);
+      rightWheel->run(FORWARD);
+    } else if (values[2] < values[1] && values[2] < values[0] && values[2] < 70) {
+      // whatever's on the right is the closest
+      leftWheel->run(FORWARD);
+      rightWheel->run(BACKWARD);
+      delay(250);
+      leftWheel->run(FORWARD);
+      rightWheel->run(FORWARD);
+    }
+  }
+}  
 
 // Move robot with arrow buttons ---------------------------------------------
 void moveRobot() {
@@ -214,13 +377,16 @@ int Sweep() {
     } else {
       dir = 1;
     }
+  } else {
+    dir = 2;
   }
   m.lcd.clear();
   if (dir == 0) {
     m.lcd.print("right");
-  }
-  else if (dir == 1) {
+  } else if (dir == 1) {
     m.lcd.print("left");
+  } else if (dir == 2) {
+    m.lcd.print("forward");
   }
   return dir;
 }
@@ -255,7 +421,6 @@ void Chicken() {
   }
 }
 
-
 void Dora() {
   m.lcd.clear();
   while(1) {
@@ -266,6 +431,8 @@ void Dora() {
 }
 //------------------------  Non-menu Functions -------------------------------
 
+
+
 // returns the range in cm
 int getRange() {
   int rangecm;
@@ -275,4 +442,24 @@ int getRange() {
   rangecm = pulseIn(4, HIGH);
   rangecm = rangecm / 58;
   return rangecm;
+}
+
+void setup() {
+  // Here is where you put lines that execute just once.  Like setting input and output.
+  m.begin();  // this starts up the menu
+  m.addItem("1 Wheel Speeds", &setWheelSpeed);    //  Here is where you put the menu items.
+  m.addItem("2 Move Robot  ", &moveRobot);       //  For each menu item, you give the text of the menu label
+  m.addItem("3 Range Finder", &testRanger);     //  and then the name of the function to be called when you select it
+  m.addItem("4 Move Servo  ", &testServo);     
+  m.addItem("5 Adjust someN", &setValue);  
+  m.addItem("6 Follower    ", &Follower);   
+  m.addItem("7 Sweep       ", &Sweep);
+  m.addItem("8 Chicken     ", &Chicken);
+
+
+  m.lcd.setBacklight(0x1);     // Turns on the backlight.  Will be done by default in next version of physMenu
+  pinMode(5, OUTPUT);  // Rangefinder trigger
+  pinMode(6, INPUT);   // Rangefinder response
+  AFMS.begin();  // Initialize the motor shield
+  rangerServo.attach(2);  // Associates rangerServo with pin 2
 }
